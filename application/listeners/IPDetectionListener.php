4041918d3bf39069eb2fc069e4fa628d5cc6a0a7
Lucian Gabriel Popescu <luciangreentree@github.com>
2018-01-29 16:10:44 +0200

    + bugfix

--- application/listeners/IPDetectionListener.php
+++ application/listeners/IPDetectionListener.php
@@ -1,62 +1,40 @@
 <?php
 require_once("hlis/ip2country/detector.php");
 require_once("application/models/dao/Countries.php");
+require_once("application/models/dao/entities/Country.php");
 
+/**
+ * Delegates to IP2COUNTRY model to detect user country based on IP from MMDB db, wraps result into a Country entity
+ * then assigns latter with country id detected from DB via Countries DAO.
+ *
+ * Saves response attributes:
+ * - ip: detected client IP, validated for proxy headers, too
+ * - country: detected Country entity
+ */
 class IPDetectionListener extends RequestListener
 {
-
     public function run()
     {
+        // sets MMDB file location
         $env = $this->application->getAttribute("environment");
         CountryDetection::$DB_FILE = (string)$this->application->getXML()->geoip->$env;
 
-        $country = CountryDetection::getInstance()->getCountry();
+        // detects and saves country
+        $countryDetected = CountryDetection::getInstance()->getCountry();
         $countries = new Countries();
-        if ($country == null) {
-            // default to USA
-            $country = $this->setDefaultCountry('US');
+        $country = new Country();
+        if ($countryDetected == null) { // if country could not be detected, defaults to US
+            $country->code = "US";
+            $country->name = "United States";
         } else {
-            $country->id = $countries->getIDByCode($country->code);
+            $country->code = $countryDetected->code;
+            $country->name = $countryDetected->name;
         }
+        $country->id = $countries->getIDByCode($country->code);
         $this->request->setAttribute('country', $country);
+
+        // detects and saves ip
         $ip = IPDetection::getInstance()->getIP();
         $this->request->setAttribute('ip', ($ip != null) ? $ip : '127.0.0.1');
-    }
-
-    private function setDefaultCountry($code)
-    {
-        $country = new Country();
-        switch ($code) {
-            case 'US':
-                $country->id = 34;
-                $country->code = "US";
-                $country->name = "United States";
-                break;
-            case 'GB':
-                $country->id = 20;
-                $country->code = "GB";
-                $country->name = "United Kingdom";
-                break;
-            case 'IL':
-                $country->id = 25;
-                $country->code = "IL";
-                $country->name = "Israel";
-                break;
-            case 'RO':
-                $country->id = 43;
-                $country->code = "RO";
-                $country->name = "Romania";
-                break;
-            case 'RU':
-                $country->id = 27;
-                $country->code = "RU";
-                $country->name = "Russian Federation";
-                break;
-            default :
-                $country->id = 34;
-                $country->code = "US";
-                $country->name = "United States";
-        }
-        return $country;
     }
 }
