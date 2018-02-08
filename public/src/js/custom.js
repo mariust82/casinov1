23e4622d49ecee12deb8f1d1db7de00dc584f016
Vadim <kvs.serve@gmail.com>
2018-02-08 13:26:16 +0200

    dev search logic

--- public/src/js/custom.js
+++ public/src/js/custom.js
@@ -1,5 +1,4 @@
 ;(function($) {
-    var PAGE = $('#conf').data('page');
     BUSY_REQUEST = false;
 
     $(document).ready(function() {
@@ -12,6 +11,7 @@
         initMobileMenu();
         copyToClipboard();
         initMoboleBonusesPop();
+        new SearchPanel ( $('.header-search') );
 
         tooltipConfig = {
             trigger: 'click',
@@ -54,6 +54,327 @@
         $('.js-tooltip-content').tooltipster(contentTooltipConfig);
 
     });
+
+    var SearchPanel = function(obj) {
+        var _obj = obj,
+            _self = this,
+            _searchButton = _obj.find('.js-trigger-search'),
+            _searchContainer = _obj.find('#search'),
+            _searchForm = _obj.find('#search-form'),
+            _searchBody = _obj.find('.search-results'),
+            _searchInput = _searchForm.find('input'),
+            _searchUpdate = _searchForm.find('.js-mobile-search-clear'), // clear text
+            _searchCasinosContainer = _searchContainer.find('#search-casinos ul'),
+            _searchPagesContainer = _searchContainer.find('#search-pages ul'),
+            _searchEmptyContainer = _searchContainer.find('#search-empty'),
+            // _searchMoreCasinos = _searchContainer.find('.js-search-more-casinos'),
+            // _searchMorePages = _searchContainer.find('.js-search-more-pages'),
+            _imgDir = _searchContainer.data('img-dir'),
+            _loadNewContent = true,
+            _loadMoreContent = false,
+            _clickCounter = 0,
+            _fromPages = 2,
+            _fromCasinos = 2,
+            loadDelay = 0,
+            // scrollingBlock = $('.js-scrolling'),
+            _request = new XMLHttpRequest();
+
+        // window.contentBeforeSearch = $('.main').html();
+
+        var _onEvent = function() {
+                // _initSearch();
+
+                _searchButton.on(
+                    'click',
+                    function() {
+                        _ajaxRequestPopup('/search');
+                        _resetPages();
+                        return false;
+                    });
+
+                _searchInput.on({
+                    'focus': function() {
+                        _ajaxRequestPopup('/search');
+                        _resetPages();
+                    },
+                    'keyup': function(e) {
+                        if (e.keyCode == 27) {
+                            _hidePanel();
+                            _resetPages();
+                        } else if (e.keyCode == 13) {
+                            // _ajaxRequestPopup('/search/advanced.json');
+                            location.href = "/search/advanced/?value="+_searchInput.val();
+                            _resetPages();
+                        } else {
+                            var searchPopup = _obj.find('#search__popup');
+                            searchPopup.addClass('load');
+                            _ajaxRequestPopup('/search');
+                            _resetPages();
+                        }
+                    }
+                });
+
+                _searchUpdate.on(
+                    'click',
+                    function() {
+                        _searchInput.val('').focus();
+                        _ajaxRequestPopup('/search');
+                        _resetPages();
+                        return false;
+                    }
+                );
+
+                // _searchMorePages.on(
+                //     'click',
+                //     function() {
+                //         _ajaxRequestPopup('/search/pages.json', _fromPages);
+                //         _loadMoreContent = true;
+                //         _fromPages++;
+                //         return false;
+                //     }
+                // );
+
+                // _searchMoreCasinos.on(
+                //     'click',
+                //     function() {
+                //         _ajaxRequestPopup('/search/casinos.json', _fromCasinos);
+                //         _loadMoreContent = true;
+                //         _fromCasinos++;
+                //         return false;
+                //     }
+                // );
+            },
+
+            _initSearch = function() {
+                var btn = $('.js-trigger-search, .search-back');
+                var searchForm = $('.search');
+                var searchFormBody = $('.search-results, .search-box, .search-input', searchForm);
+
+                btn.on('click', function(e) {
+                    setTimeout(function() {
+                        $('body').toggleClass('search-expanded');
+                        searchForm.find('.search-textfield').focus();
+                    }, 200);
+
+                    $('.js-trigger-search').toggleClass('active');
+
+                    searchForm.css({
+                        'left': btn[0].getBoundingClientRect().right - searchForm.width(),
+                        'right': 'auto'
+                    });
+
+                    $(document).on('click touchstart', function(e) {
+                        if ($(e.target).closest(searchFormBody).length == 0 && $(e.target).closest(btn).length == 0) {
+                            $("body").removeClass('search-expanded');
+                            btn.removeClass('active');
+                        }
+                    });
+                    e.preventDefault();
+                });
+            },
+            _clearSearchBody = function() {
+                _searchCasinosContainer.empty();
+                _searchPagesContainer.empty();
+            },
+            _ajaxRequestPopup = function(target, page) {
+                if (BUSY_REQUEST) return;
+                BUSY_REQUEST = true;
+                _request.abort();
+                _request = $.ajax({
+                    url: target,
+                    data: {
+                        value: _searchInput.val(),
+                        page: page
+                    },
+                    dataType: 'json',
+                    type: 'GET',
+                    success: function(data) {
+                        _hideLoading();
+                        _loadData(data);
+                    },
+                    error: function(XMLHttpRequest) {
+                        if (XMLHttpRequest.statusText != "abort") {
+                            _hideLoading();
+                            console.log('err');
+                            _showEmptyMessage();
+                        }
+                    },
+                    complete: function() {
+                        _hideLoading();
+                        BUSY_REQUEST = false;
+                    }
+                });
+
+                var loadDelay = setTimeout(function() {
+                    _searchContainer.addClass('loading');
+                }, 300);
+
+                _hideLoading = function(){
+                    clearTimeout(loadDelay);
+                    _searchContainer.removeClass('loading');
+                }
+            },
+
+            _loadData = function(data) {
+                var casinos = data.body.casinos;
+                var pages = data.body.games;
+
+                if (!_loadMoreContent) {
+                    _clearSearchBody();
+                }
+
+                if ($.isEmptyObject(casinos) && $.isEmptyObject(pages)) {
+                    _showEmptyMessage();
+                } else {
+                    _hideEmptyMessage();
+
+                    if (!$.isEmptyObject(casinos)) {
+                        _searchCasinosContainer
+                            .parent()
+                            .show()
+                            .next()
+                            .removeClass('single');
+
+                        if (data.body.total_casinos > 3 && Math.ceil(data.body.total_casinos / 3) > _fromCasinos) {
+                            // _searchMoreCasinos.show();
+                        } else {
+                            // _searchMoreCasinos.hide();
+                            _fromCasinos = 2;
+                        }
+                    } else {
+                        _searchCasinosContainer
+                            .parent()
+                            .hide()
+                            .next()
+                            .addClass('single');
+                    }
+
+                    if (!$.isEmptyObject(pages)) {
+                        _searchPagesContainer
+                            .parent()
+                            .show()
+                            .prev()
+                            .removeClass('single');
+                        if (data.body.total_pages > 3 && Math.ceil(data.body.total_casinos / 3) > _fromPages) {
+                            // _searchMorePages.show();
+                        } else {
+                            // _searchMorePages.hide();
+                            _fromPages = 2;
+                        }
+                    } else {
+                        _searchPagesContainer
+                            .parent()
+                            .hide()
+                            .prev()
+                            .addClass('single');
+                    }
+
+                    function getWebName(name) {
+                        return name.replace(/\s/g, '_').toLowerCase();
+                    }
+
+                    function getItemPattern(itemData) {
+                        var pattern = '<li>\
+                            <a href="/'+itemData.link+'">\
+                                '+itemData.name+'\
+                            </a>\
+                        </li>';
+
+
+                        return pattern;
+                    }
+
+                    for (var casino in casinos) {
+
+                        var _item = getItemPattern({
+                            link: casinos[casino].url,
+                            img: _imgDir + '/casino_logo_light/124x82/' + casinos[casino].code + '.png',
+                            name: casinos[casino].name
+                        });
+                        _searchCasinosContainer.append(_item);
+                    }
+
+                    for (var page in pages) {
+
+                        var _item = getItemPattern({
+                            link: pages[page].url,
+                            // img: _imgDir + '/top10_search_pages/' + getWebName(pages[page].name) + '.png',
+                            name: pages[page].name
+                        });
+
+                        _searchPagesContainer.append(_item);
+                    }
+                }
+                // _updateCustomScroll();
+                _illumination();
+            },
+
+            _resetPages = function() {
+                _fromPages = 2;
+                _fromCasinos = 2;
+            },
+
+            _updateCustomScroll =function(){
+                var headHeight = $('.search-box').outerHeight();
+                var wh = $(window).height()-headHeight;
+                if (wh > 652) {
+                    wh = 652;
+                }
+
+                scrollingBlock.mCustomScrollbar('destroy');
+                scrollingBlock.mCustomScrollbar('update');
+                setTimeout(function() {
+                    scrollingBlock.mCustomScrollbar({
+                      set_height:wh,
+
+                      scrollInertia: 0,
+                      // autoHideScrollbar: true,
+                      advanced: {
+                          updateOnContentResize: true
+                      }
+                    });
+
+                    if (_loadMoreContent) {
+                        scrollingBlock.mCustomScrollbar("scrollTo", "bottom");
+                    }
+                    _loadMoreContent = false;
+                }, 0);
+            },
+            _showEmptyMessage = function(){
+                _searchCasinosContainer.parent().hide();
+                _searchPagesContainer.parent().hide();
+                _searchEmptyContainer.show();
+            },
+            _hideEmptyMessage = function(){
+                _searchCasinosContainer.parent().show();
+                _searchPagesContainer.parent().show();
+                _searchEmptyContainer.hide();
+            },
+            _hidePopup = function() {
+                $('body').removeClass('search-expanded');
+                _loadNewContent = true;
+            },
+            _hidePanel = function() {
+                _hidePopup();
+            },
+            _illumination = function() {
+                var searchItems = _obj.find('.search-results-label');
+                searchItems.each(function() {
+                    $(this).html(function(_, html) {
+                        return html.replace(new RegExp(_searchInput.val().toLowerCase(), 'i\g'), '<b>$&</b>')
+                    });
+                });
+            },
+            _construct = function() {
+                _onEvent();
+                _obj[0].obj = _self;
+            };
+        _self.close = function() {
+            _hidePanel();
+        };
+
+        _construct();
+    };
 
     function initMoboleBonusesPop() {
         var _container = $('.list-item');
