b9ba9dbb4b53f596144d7586c811bcabb9a4c340
Vadim <kvs.serve@gmail.com>
2018-02-13 14:21:52 +0200

    + implement menus

--- application/controllers_frontend/CompatibilityController.php
+++ application/controllers_frontend/CompatibilityController.php
@@ -1,6 +1,56 @@
 <?php
 class CompatibilityController extends Controller {
     public function run() {
+        $this->response->setAttribute("menu_top", array (
+  0 => 
+  array (
+    'title' => 'CASINOS',
+    'url' => '/casinos',
+    'is_active' => false,
+  ),
+  1 => 
+  array (
+    'title' => 'SOFTWARES',
+    'url' => '/softwares',
+    'is_active' => false,
+  ),
+  2 => 
+  array (
+    'title' => 'BONUSES',
+    'url' => '/bonus-list',
+    'is_active' => false,
+  ),
+  3 => 
+  array (
+    'title' => 'COUNTRIES',
+    'url' => '/countries',
+    'is_active' => false,
+  ),
+  4 => 
+  array (
+    'title' => 'COMPATIBILITY',
+    'url' => '/compatability',
+    'is_active' => true,
+  ),
+  5 => 
+  array (
+    'title' => 'BANKING',
+    'url' => '/banking',
+    'is_active' => false,
+  ),
+  6 => 
+  array (
+    'title' => 'FEATURES',
+    'url' => '/features',
+    'is_active' => false,
+  ),
+  7 => 
+  array (
+    'title' => 'GAMES',
+    'url' => '/games',
+    'is_active' => false,
+  ),
+));
         $this->response->setAttribute("results", array (
   'Mac' => '1088',
   'Linux' => '1026',
