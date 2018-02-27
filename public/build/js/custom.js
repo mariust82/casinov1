f0f946da89c0fef98db5f74ed0bca24ec170ffad
Vadim <kvs.serve@gmail.com>
2018-02-27 15:51:26 +0200

    + reviewer flag, -reply button

--- public/build/js/custom.js
+++ public/build/js/custom.js
@@ -871,7 +871,8 @@
                     var pattern;
                     var getLink = function(){
                         if (_field_name.closest('.reply').parent('#review-data-holder').length > 0) {
-                            return '<a href="#" class="review-replies js-reply-btn">Reply</a>';
+                            // return '<a href="#" class="review-replies js-reply-btn">Reply</a>';
+                            return '';
                         } else {
                             return '';
                         }
