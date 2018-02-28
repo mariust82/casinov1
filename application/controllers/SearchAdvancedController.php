9930183734ed677426e999a19d63f3fad695e367
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-02-28 12:23:18 +0200

    * search advanced no longer a page

--- application/controllers/SearchAdvancedController.php
+++ application/controllers/SearchAdvancedController.php
@@ -1,13 +1,12 @@
 <?php
 require_once("application/models/dao/CasinosSearch.php");
 require_once("application/models/dao/GamesSearch.php");
-require_once("application/models/dao/TopMenu.php");
 
 /*
 * Page to display after show all results is clicked @ search page
 * 
 * @requestMethod GET
-* @responseFormat HTML
+* @responseFormat JSON
 * @source https://xd.adobe.com/view/7bbdd623-2cdd-4cf4-971f-98d886e7a2b8/screen/015eebd7-3b58-43a8-a8db-24dd6f810135/Search?fullscreen
 * @requestParameter value string Value of searched string
 */
@@ -16,9 +15,6 @@ class SearchAdvancedController extends Controller {
 
 	public function run() {
 	    $this->response->setAttribute("value", $_GET["value"]);
-
-        $menu = new TopMenu($this->request->getValidator()->getPage());
-        $this->response->setAttribute("menu_top", $menu->getEntries());
 
         $casinos = new CasinosSearch($_GET["value"]);
         $this->response->setAttribute("casinos", $casinos->getResults(self::LIMIT,0));
