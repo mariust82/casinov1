f13cb55f48742b0e8970adbb455fb75bb9d9a1d2
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-03-05 15:29:09 +0200

    * tryfix

--- application/controllers/sitemap/AbstractSitemapController.php
+++ application/controllers/sitemap/AbstractSitemapController.php
@@ -1,8 +1,6 @@
 <?php
 abstract class AbstractSitemapController extends Controller
 {
-    const SITE_URL = "www.casinoslists.com";
-
     public function run() {
         $this->response->setAttribute("pages", $this->getPages());
         $this->response->setAttribute("priority", $this->getPriority());
@@ -14,7 +12,7 @@ abstract class AbstractSitemapController extends Controller
         $items = $this->getItems();
         $pages = array();
         foreach($items as $name) {
-            $pages[] = $protocol."://".self::SITE_URL."/".strtolower(str_replace(" ", "-", str_replace("(item)", $name, $urlPattern)));
+            $pages[] = $protocol."://".$this->request->getServer()->getName()."/".strtolower(str_replace(" ", "-", str_replace("(item)", $name, $urlPattern)));
         }
         return $pages;
     }
