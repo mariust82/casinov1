edce4052cb6017fbe53c9ccc28f32c66fc215551
shaykeman <shay@hliscorp.com>
2018-04-17 09:46:53 -0400

    change to backed

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ new ErrorsFrontController();
 // take control of STDOUT
 require_once("vendor/lucinda/mvc/loader.php");
 try {
-    new FrontController("configuration_frontend.xml");
+    new FrontController("configuration.xml");
 } catch(PathNotFoundException $e) {
     $_SERVER["REQUEST_URI"] = "/404";
-    new FrontController("configuration_frontend.xml");
+    new FrontController("configuration.xml");
 }
