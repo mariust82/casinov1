bed5303bf5a3bc879a0504ae946b35b5cbb6b5c8
shaykeman <shay@hliscorp.com>
2018-04-25 11:34:21 +0300

    fix bug

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -26,7 +26,12 @@ class IndexController extends Controller {
         $this->response->setAttribute("country_casinos", $this->getCasinos(array("country_accepted"=>1, "promoted"=>1), CasinoSortCriteria::POPULARITY, 5));
         $this->response->setAttribute("new_casinos", $this->getCasinos(array("country_accepted"=>1, "promoted"=>1), CasinoSortCriteria::NEWEST, 5));
         $this->response->setAttribute("no_deposit_casinos", $this->getCasinos(array("country_accepted"=>1, "promoted"=>1,"bonus_type"=>"No Deposit Bonus"), CasinoSortCriteria::NEWEST, 5));
-        $object = new GamesList(new GameFilter(array(), $this->request->getAttribute("country")));
+        $is_mobile = $this->request->getAttribute("is_mobile");
+        $game_filter = new GameFilter(array("game_type"=>$this->response->getAttribute("selected_entity")));
+        if ($is_mobile) {
+            $game_filter->is_mobile = TRUE;
+        }
+        $object = new GamesList($game_filter);
         $this->response->setAttribute("new_games", $object->getResults(GameSortCriteria::NEWEST, 0,6));
 
         $object = new PageInfoDAO();
