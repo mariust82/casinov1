e67b0c329c18830e41b050ef698ab436d07ac043
shaykeman <shay@hliscorp.com>
2018-04-30 10:58:48 +0300

    CASLI-152 fix sitemap

--- application/controllers/sitemap/PagesController.php
+++ application/controllers/sitemap/PagesController.php
@@ -3,7 +3,7 @@ require_once("AbstractSitemapController.php");
 
 class PagesController extends AbstractSitemapController {
     protected function getItems() {
-        return ["Casinos","Softwares","Bonus List","Countries","Compatability","Banking","Features","Games"];
+        return ["Casinos","Softwares","Bonus List","Countries","Banking","Games"];
     }
 
     protected function getUrlPattern()
