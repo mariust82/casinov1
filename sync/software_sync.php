a40bdc447608a7afb3a4b8aad163e817368d1797
shaykeman <shay@hliscorp.com>
2018-05-16 12:11:14 +0300

    CASLI-189

--- sync/software_sync.php
+++ sync/software_sync.php
@@ -2,6 +2,6 @@
 set_include_path(dirname(__DIR__)."/hlis/sync");
 
 require_once("src/controllers/TableSynchronization.php");
-require_once(__DIR__."/classes/CasinoproGameManufacturesSynchronization.php");
+require_once(__DIR__."/classes/CasinoslistsGameManufacturesSynchronization.php");
 
 new CasinoslistsGameManufacturesSynchronization(dirname(__FILE__)."/configuration.xml");
