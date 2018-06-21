4f90b11a985bc606f211baf1ee94648c75e01c04
Alexandru Dumitrescu <alexandru.d@hliscorp.com>
2018-06-21 17:54:03 +0300

    (TMS) - Minor Fix

--- application/models/tms_variables/FirstDepositBonus.php
+++ application/models/tms_variables/FirstDepositBonus.php
@@ -14,7 +14,7 @@ class FirstDepositBonus extends \TMS\VariablesHolder {
         $filter = new CasinoFilter($filterParams, $this->parameters["response"]->getAttribute("country"));
         $object = new CasinosList($filter);
 
-        $casino = $object->getResults(CasinoSortCriteria::POPULARITY, 0, 1);
+        $casino = $object->getResults(CasinoSortCriteria::NONE, 0, 1);
         if($casino[0] && $casino[0]->bonus_first_deposit->amount){
             return $casino[0]->bonus_first_deposit->amount;
         }
