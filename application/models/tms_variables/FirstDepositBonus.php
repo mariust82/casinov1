87c62d7d99a3d9e328a85538b479eb4a30ec4bf7
Alexandru Dumitrescu <alexandru.d@hliscorp.com>
2018-06-21 18:20:36 +0300

    Minor Fix

--- application/models/tms_variables/FirstDepositBonus.php
+++ application/models/tms_variables/FirstDepositBonus.php
@@ -10,11 +10,10 @@ class FirstDepositBonus extends \TMS\VariablesHolder {
 
     public function getBestInList(){
         $filterParams[$this->parameters["response"]->getAttribute("filter")] = $this->parameters["response"]->getAttribute("selected_entity");
-        $filterParams["label"] = 'Best';
         $filter = new CasinoFilter($filterParams, $this->parameters["response"]->getAttribute("country"));
         $object = new CasinosList($filter);
 
-        $casino = $object->getResults(CasinoSortCriteria::NONE, 0, 1);
+        $casino = $object->getResults(CasinoSortCriteria::TOP_RATED, 0, 1);
         if($casino[0] && $casino[0]->bonus_first_deposit->amount){
             return $casino[0]->bonus_first_deposit->amount;
         }
