1af00e1ad8415ef592c649a13a6821959c65dd78
adrianm80 <38781471+adrianm80@users.noreply.github.com>
2018-08-24 16:49:09 +0300

    CASLI-246
    New and Live Dealer casino categories display contradicting results - also keep the actual order

--- application/models/dao/CasinoLabels.php
+++ application/models/dao/CasinoLabels.php
@@ -4,30 +4,56 @@ require_once("FieldValidator.php");
 
 class CasinoLabels implements CasinoCounter, FieldValidator
 {
+
+    private static $orderLabels = [
+        'Stay away',
+        'New',
+        'Best',
+        'Recommended',
+        'Popular',
+        'Mobile',
+        'Live Dealer',
+        'eCOGRA'
+    ];
+
     public function getCasinosCount() {
+
         $labels =  DB("
         SELECT
         t1.name AS unit, count(*) as counter
         FROM casino_labels AS t1
         INNER JOIN casinos__labels AS t2 ON t1.id = t2.label_id
         INNER JOIN casinos AS t3 ON t2.casino_id = t3.id
-        WHERE t3.is_open = 1 AND t1.id != 8
+        WHERE t3.is_open = 1 AND t1.id != 8 AND t1.id != 3
         GROUP BY t1.id
         ORDER BY counter DESC 
         ")->toMap("unit","counter");
-        
+
+
+
+        $new_casinos_nr = DB("
+          SELECT DISTINCT COUNT(t1.id) AS counter
+           FROM casinos AS t1 
+            WHERE t1.is_open = 1 AND t1.date_established > DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+        ")->toValue();
+
+        $new_casinos['New'] = $new_casinos_nr;
+        $array = array_merge($labels, $new_casinos);
+
         $mobile =  DB("
-        SELECT
+        SELECT DISTINCT 
         t1.name AS unit, count(*) as counter
+       
         FROM play_versions AS t1
         INNER JOIN casinos__play_versions AS t2 ON t1.id = t2.play_version_id
         INNER JOIN casinos AS t3 ON t2.casino_id = t3.id
-        WHERE t3.is_open = 1 AND t1.id = 4 OR t1.id = 2
+        WHERE t3.is_open = 1 AND (t1.id = 4 OR t1.id = 2)
         GROUP BY t1.id
         ORDER BY counter DESC 
         ")->toMap("unit","counter");
-        $array = array_merge($labels,$mobile);
-        
+
+        $array = array_merge($array, $mobile );
+
         $features =  DB("
         SELECT
         t1.name AS unit, count(*) as counter
@@ -38,11 +64,30 @@ class CasinoLabels implements CasinoCounter, FieldValidator
         GROUP BY t1.id
         ORDER BY counter DESC 
         ")->toMap("unit","counter");
-        
-        return array_merge($array,$features);
+
+        $labels = array_merge($array,$features);
+        $orderLabels =  $this->orderLabels($labels);
+
+        return $orderLabels;
     }
 
     public function validate($name) {
         return DB("SELECT name FROM casino_labels WHERE name=:name",array(":name"=>$name))->toValue();
+    }
+
+    private function orderLabels(array $labels){
+
+        if(empty($labels)){
+            throw new Exception('Labels should not be emtpy');
+        }
+
+        $orderedLabels = [];
+        foreach(self::$orderLabels as $key => $label){
+
+            if(isset($labels[$label]))
+                $orderedLabels[$label] = $labels[$label];
+        }
+
+        return $orderedLabels;
     }
 }
