19f8a8cf8c87e4cf4c85e3be5e5d1dd4d1e1facf
adrianm80 <38781471+adrianm80@users.noreply.github.com>
2018-08-29 09:55:29 +0300

    CASLI-234
    update

--- application/models/tms_variables/NoDepositBonusTms/NoDepositBonusData.php
+++ application/models/tms_variables/NoDepositBonusTms/NoDepositBonusData.php
@@ -1,16 +1,20 @@
 <?php
 
 require_once 'application/models/tms_variables/NoDepositBonusTms/NoDepositBonusInterface.php';
+require_once 'application/models/CasinoFilter.php';
+require_once 'application/models/CasinoSortCriteria.php';
+require_once 'application/models/dao/entities/Country.php';
+require_once 'application/models/dao/CasinosList.php';
 
 class NoDepositBonusData implements  NoDepositBonusInterface{
 
     private $filter = [];
     private $country ='';
 
-    public function __construct($country){
+    function __construct($country)
+    {
 
         $this->country = $country;
-
     }
 
     public function setFilter($filter)
@@ -18,10 +22,12 @@ class NoDepositBonusData implements  NoDepositBonusInterface{
         $this->filter = new CasinoFilter($filter, $this->country);
     }
 
-    function getData($sortCriteria )
+    public function getData($sortCriteria)
     {
+
         $casino = new CasinosList($this->filter);
         $casino_data =  $casino->getResults($sortCriteria, null, 1);
         return $casino_data;
     }
+
 }
