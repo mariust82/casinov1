98263b6bca54406e3552d2e4b20d45ce8546e600
Mihai Balalau <mihai.b@hliscorp.com>
2018-09-20 11:59:28 +0300

    CASLI-256

---
+++ application/models/tms_variables/Visits.php
@@ -0,0 +1,35 @@
+<?php
+
+class Visits extends \TMS\VariablesHolder {
+    public function getNewestGameTotal() {
+        $query = "SELECT times_played FROM games ORDER BY date DESC LIMIT 1";
+        return DB($query)->toValue();
+    }
+
+    public function getBestGameTotal() {
+        $query = "SELECT times_played FROM games ORDER BY times_played DESC LIMIT 1";
+        return DB($query)->toValue();
+    }
+
+    public function getBestCasinoTotal() {
+        $country = $this->getCountry();
+        $filter = new CasinoFilter(['label' => 'best'], $country);
+        $casinos = new CasinosList($filter);
+
+        $result = $casinos->getResults(CasinoSortCriteria::TOP_RATED,1, 5);
+        return $result[0]->clicks . " - " . $result[0]->name;
+    }
+
+    public function getNewestCasinoTotal() {
+        $country = $this->getCountry();
+        $filter = new CasinoFilter(['label' => 'new'], $country);
+        $casinos = new CasinosList($filter);
+        $result = $casinos->getResults(CasinoSortCriteria::NEWEST, 1, 1);
+
+        return $result[0]->clicks . " - " . $result[0]->name;
+    }
+
+    private function getCountry() {
+        return $this->parameters['response']->getAttribute('country');
+    }
+}
