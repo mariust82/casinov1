9ab9bf30abeb98444dcced01a5c49376f65065ef
Alexandru Dumitrescu <alexandru.d@hliscorp.com>
2018-09-26 12:30:26 +0300

    CASLI-208, CASLI-207

---
+++ public/build/js/fluid-gameplay.js
@@ -0,0 +1,36 @@
+(function($) {
+
+    var ResizeGameplay = function (iframe) {
+        // Find iframe
+        var $iframe = $(iframe);
+
+        //Calculate the height of extra elements
+        var $elements = $iframe.parent().children().not(iframe);
+        var elHeight = 0;
+        $elements.each(function () {
+            elHeight += $(this).height();
+        });
+
+        // Find and save the aspect ratio for iframe
+        $iframe.data("ratio", $iframe.height() / $iframe.width()).removeAttr("width").removeAttr("height");
+
+        // Resize the iframe when the window is resized
+        $(window).resize(function () {
+
+            //Get the parent container's width and height
+            var width = $iframe.parent().width();
+            var height = $iframe.parent().outerHeight() - elHeight;
+
+            //Adding width and height on iframe keeping proportions
+            if ($iframe.data("ratio") < height / width) {
+                $iframe.width(width).height(width * $iframe.data("ratio"));
+            } else {
+                $iframe.width(height / $iframe.data("ratio")).height(height);
+            }
+
+            // Resize to fix iframe on page load.
+        }).resize();
+    }
+
+    ResizeGameplay("iframe");
+})
