081b7e8986a004cf8a9aed4721e1585e2f362ab5
Alexandru Dumitrescu <alexandru.d@hliscorp.com>
2018-10-01 15:15:25 +0300

    LISTS-232

--- public/build/js/custom.js
+++ public/build/js/custom.js
@@ -833,13 +833,13 @@ var AJAX_CUR_PAGE = 1;
             console.log($iframe.contents().readyState);
             var gameUrl = $iframe.contents().find("#overlay").attr('data-game-url');
             //var gameUrl = $(this).contents().find("[data-game-url]");
-            if(gameUrl.length > 0){
+            //if(gameUrl.length > 0){
                 var iframePlayButton = $(this).contents().data("#game_play_button");
                 $(iframePlayButton).on('click', function(){
                     $iframe.attr('src',gameUrl);
                     that.toogleFullscreen();
                 })
-            }
+            //}
         }
 
         function getExtraHeight() {
