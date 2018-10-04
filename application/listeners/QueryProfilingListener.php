429d51ffdd0e56218101ce8167abcef2a6a69263
CasinosLists <server@hliscorp.com>
2018-10-04 08:03:03 -0400

    bugfix

--- application/listeners/QueryProfilingListener.php
+++
@@ -1,9 +0,0 @@
-<?php
-class QueryProfilingListener extends RequestListener {
-	public function run() {
-		// here you can add no matter how many environments you want
-		if($this->application->getAttribute("environment")=="dev") {
-			 require_once("hlis/sitebase/QueryProfiler.php");
-		}
-	}
-}
