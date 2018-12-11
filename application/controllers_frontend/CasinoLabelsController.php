2fc482220dd1039aca7f519a9c1dae6f36e7573e
CasinosLists <server@hliscorp.com>
2018-12-11 08:41:00 -0500

    front end stuff

--- application/controllers_frontend/CasinoLabelsController.php
+++ application/controllers_frontend/CasinoLabelsController.php
@@ -1,5 +1,5 @@
 <?php
-class PageNotFoundController extends Controller {
+class CasinoLabelsController extends Controller {
     public function run() {
         $this->response->setAttribute("country", array (
   'id' => '43',
@@ -31,7 +31,7 @@ $this->response->setAttribute("menu_top", array (
   array (
     'title' => 'CASINOS',
     'url' => '/casinos',
-    'is_active' => false,
+    'is_active' => true,
     'submenuItems' => 
     array (
       'Best Casinos' => '/casinos/best',
@@ -123,14 +123,32 @@ $this->response->setAttribute("menu_top", array (
     'have_submenu' => true,
   ),
 ));
+$this->response->setAttribute("results", array (
+  'Best' => '87',
+  'Live Dealer' => '461',
+  'Mobile' => '896',
+  'eCOGRA' => '137',
+  'Stay away' => '42',
+  'Popular' => '15',
+));
+$this->response->setAttribute("tms", array (
+  'upper_text' => '<p>How to choose an online casino? What are the most important criteria you should consider before embarking on the gambling arena? And who else better than us to guide you through this strategic choice? We at CasinosLists.com reveal what counts in a decision that it should not have anything to chance! Games library, bonuses, license - all points that we check for each of the casinos presented in our listing. It may be tempting to choose the first casino you see in the search engine, but it can be a serious mistake, that&rsquo;s why you need to know exactly what you are looking for at an online casino. We are up to date with all the <a href="https://www.casinoslists.com/casinos/new">New Online Casinos</a> out there, so you can be tuned in with all the new bonuses and promotions.</p>
+
+<p>&nbsp;</p>
+
+<p>Of all these casinos, which one will be your favourite? To find that out...test them all. Sign up, make a deposit if required, test the demos and enjoy the bonuses: it&#39;s the best way to make an informed choice. Scroll away through our list of <a href="https://www.casinoslists.com/casinos/best">Best Online Casinos</a>, read the reviews and comments and decide upon the online casino that meets your expectations. The second step is to play only at <a href="https://www.casinoslists.com/features/ecogra-casinos">eCOGRA Online Casinos</a>, an institution aimed at regulating and ensuring online gambling operators. Its goal is to control players in the online gaming market and protect users. Online gaming operators with eCOGRA approval stamp must adhere to strict standards of behavior. On the opposite, you should avoid casinos that were blacklisted for one reason or another. In this regard, CasinosLists.com will help you do so by providing you with the <a href="https://www.casinoslists.com/casinos/stay-away">Stay Away Online Casinos</a>.</p>
+
+<p>&nbsp;</p>
+
+<p>Our <a href="https://www.casinoslists.com/features/live-dealer">Live Dealer Casinos</a> and <a href="https://www.casinoslists.com/casinos/popular">Popular Casinos</a> lists will offer you a preview of the most popular options in the online gaming market. Players participate in the action through video streaming and often place their bets via their mobile devices. You should check CasinosLists&rsquo; <a href="https://www.casinoslists.com/compatability/mobile">Mobile Casinos</a> page to find out which online gambling sites are compatible with your operating system. Softwares like RTG or Microgaming even allow gamblers to chat with each other or chat with the dealer via the Live Chat feature. There are plenty of options, so don&rsquo;t hold back and give them a try! Good luck!</p>
+',
+));
 $this->response->setAttribute("page_info", array (
-  'head_title' => 'CasinosLists.com 404 Not Found',
-  'head_description' => '',
-  'body_title' => '',
+  'head_title' => ' Online Casinos Categories - CasinosLists.com',
+  'head_description' => 'Complete List of Online Casinos Categories | Check out various types of Online Gaming Sites including Best Casinos, Live Casinos or Mobile-Friendly platforms',
+  'body_title' => 'Online Casinos Categories',
 ));
 $this->response->setAttribute("version", '0.8.3.7');
-$this->response->setAttribute("tms", array (
-));
 
     }
 }
