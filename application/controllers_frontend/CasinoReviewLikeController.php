1020c9a9688258239fe019f56f4c7881e329b907
CasinosLists <server@hliscorp.com>
2018-12-11 08:56:16 -0500

    frontend controllers

---
+++ application/controllers_frontend/CasinoReviewLikeController.php
@@ -0,0 +1,5 @@
+<?php
+class CasinoReviewLikeController extends Controller {
+    public function run() {
+    }
+}
---
+++ application/controllers_frontend/CasinoReviewWriteController.php
@@ -0,0 +1,6 @@
+<?php
+class CasinoReviewWriteController extends Controller {
+    public function run() {
+        $this->response->setAttribute("id", 10);
+    }
+}
