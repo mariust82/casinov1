80f3134402b62158630866c5bef10da3da65312d
adrianm80 <38781471+adrianm80@users.noreply.github.com>
2019-01-08 17:46:12 +0200

    update / add fix

--- public/build/js/custom.js
+++ public/build/js/custom.js
@@ -2137,7 +2137,7 @@ var AJAX_CUR_PAGE = 1;
                                     if (flag) {
                                         flag = false;
                                         if ($(window).width() > 480) {
-                                            symbolsCount += 250;
+                                            symbolsCount += 300;
                                         }
                                         itemText = childs[i].innerHTML.substring(0,symbolsCount) + '<span class="read_controll">...</span>';
                                         createTextParagraf(itemText,childsHolder,childs[i]);
