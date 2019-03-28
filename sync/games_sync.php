af530dc439ae90d864a48f7d80c66e76594f0390
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-03-28 12:27:49 +0200

    *CASLI-705

--- sync/games_sync.php
+++ sync/games_sync.php
@@ -2,4 +2,4 @@
 ini_set('memory_limit', '1000M');
 set_include_path(dirname(__DIR__) . "/hlis/sync");
 require_once("src/controllers/NewGameSynchronization.php");
-new NewGameSynchronization(dirname(__FILE__) . "/configuration.xml", true);
+new NewGameSynchronization(dirname(dirname(__FILE__)) . "/configuration.xml", true);
--- sync/images_sync.php
+++ sync/images_sync.php
@@ -2,4 +2,4 @@
 set_include_path(dirname(__DIR__)."/hlis/sync");
 
 require_once("src/controllers/ImageSynchronization.php");
-new ImageSynchronization(dirname(__FILE__)."/configuration.xml", true);
+new ImageSynchronization(dirname(dirname(__FILE__))."/configuration.xml", true);
