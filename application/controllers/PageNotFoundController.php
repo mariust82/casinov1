59b28e718b8cbac5bc5245d5f0ccd79be700d5db
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-05-24 10:24:01 +0300

    * bugfix

--- application/controllers/PageNotFoundController.php
+++ application/controllers/PageNotFoundController.php
@@ -1,12 +1,21 @@
 <?php
-require_once("BaseController.php");
+require_once ("application/models/dao/PageInfoDAO.php");
+require_once ("application/models/dao/TopMenu.php");
 
-class PageNotFoundController extends BaseController{
-    public function service() {
-        $this->response->setStatus(404);
-    }
-    protected function pageInfo(){
+class PageNotFoundController extends Lucinda\MVC\STDERR\Controller {
+    public function run(){
         $object = new PageInfoDAO();
-        $this->response->attributes()->set("page_info", $object->getInfoByURL($this->request->getValidator()->getPage()));
+        $this->response->setAttribute("page_info", $object->getInfoByURL("404"));
+
+        $this->response->setAttribute("use_bundle", (in_array(ENVIRONMENT, ["dev","live"])?true:false));
+
+        $this->response->setAttribute("version", (string)$this->application->getTag('application')['version']);
+
+        $country = new Country();
+        $country->code = "US";
+        $country->name = "United States";
+
+        $menu = new TopMenu("404", substr($_SERVER["REQUEST_URI"],1), $country);
+        $this->response->setAttribute("menu_top", $menu->getEntries());
     }
 }
