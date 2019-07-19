6153290dbc4f5b5dfb2f84d9015afc0d382d5b0d
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-07-19 16:09:28 +0300

    fix

--- application/controllers/CasinoBonusController.php
+++ application/controllers/CasinoBonusController.php
@@ -5,7 +5,7 @@ class CasinoBonusController extends Lucinda\MVC\STDOUT\Controller {
     public function run() {
         $object = new Casinos();
         $casinoID = $this->request->attributes('validation_results')->get('casino');
-
+        $this->response->attributes("name", $this->request->parameters("casino"));
         $this->response->attributes("bonus", $object->getBonus($casinoID, (boolean) $this->request->parameters("is_free")));
     }
 }
