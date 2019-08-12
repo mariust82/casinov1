2acb2209192b35c115bdf32076fb0cdd640eb7fc
shaykeman <shay@hliscorp.com>
2019-08-12 13:29:42 +0300

    debug casli replies

--- application/controllers/CasinoMoreReviewsController.php
+++ application/controllers/CasinoMoreReviewsController.php
@@ -20,6 +20,7 @@ class CasinoMoreReviewsController extends Lucinda\MVC\STDOUT\Controller {
 
         // get reviews
         $object = new CasinoReviews();
-        $this->response->attributes("reviews", $object->getAll($casinoID, (integer) $this->request->getValidator()->parameters("page"), (integer) $this->response->attributes("id")));
+        var_dump($this->request->parameters("id"));
+        $this->response->attributes("reviews", $object->getAll($casinoID, (integer) $this->request->getValidator()->parameters("page"), (integer) $this->request->parameters("id")));
 	}
 }
