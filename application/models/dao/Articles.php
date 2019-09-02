d0574693965eb0f11c8454e5d9b536912c694632
shaykeman <shay@hliscorp.com>
2019-09-02 18:38:18 +0300

    fix comments

--- application/models/dao/Articles.php
+++ application/models/dao/Articles.php
@@ -88,7 +88,7 @@ class Articles
         $select->orderBy()->add("a.id",Lucinda\Query\OrderByOperator::DESC);
         $select->limit($limit, $offset);
         echo $select->toString();
-        var_dump($select->toString());
+        var_dump($query_vars);
         $resultSet = SQL($select->toString(), $query_vars);
         $foundRows = (int)SQL('SELECT FOUND_ROWS()')->toValue();
         $results = [];
