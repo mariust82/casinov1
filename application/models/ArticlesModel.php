f7845686b4c9488c929ed8f10a7f2f07bda3e5d2
shaykeman <shay@hliscorp.com>
2019-09-03 19:54:05 +0300

    remove test query

--- application/models/ArticlesModel.php
+++ application/models/ArticlesModel.php
@@ -52,7 +52,7 @@ class ArticlesModel {
             $uploadsFolders[$item->id] = "/upload" . $this->uploadFodler;
             $uploadsFolders[$item->id] .= "/" . str_replace(" ", "_", $item->title) . "_thumbnail.jpg?" . strtotime("now");
         }
-        
+        var_dump($uploadsFolders);
         return $uploadsFolders;
     }
     
