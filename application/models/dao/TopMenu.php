5265bde74398458af841a99636d78e2fcd12baad
shaykeman <shay@hliscorp.com>
2019-09-04 15:28:39 +0300

    debug top menu

--- application/models/dao/TopMenu.php
+++ application/models/dao/TopMenu.php
@@ -172,7 +172,7 @@ class TopMenu
                     $si = new MenuItem();
                     $si->title = $subItemTitle;
                     $si->url = $subItemUrl;
-                    var_dump(""/".$specific_page");
+                    var_dump("/".$specific_page);
                     var_dump($subItemUrl);
                     $si->is_active = ("/".$specific_page==$subItemUrl?true:false);
                     $object->submenuItems[] = $si;
