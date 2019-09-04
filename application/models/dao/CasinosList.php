e6dbbaf1c21a47d57db1d3e8bbe02402c8e6adea
shaykeman <shay@hliscorp.com>
2019-09-04 17:12:36 +0300

    debug

--- application/models/dao/CasinosList.php
+++ application/models/dao/CasinosList.php
@@ -118,7 +118,6 @@ class CasinosList
         // build query
         $queryGenerator = new CasinosListQuery($this->filter, array("COUNT(t1.id) AS nr"), null, 0, '', false);
         $query = $queryGenerator->getQuery();
-        echo $query;
         return SQL($query)->toValue();
     }
 
