30e518f93d86d404b4fb98e78570e0ef1be18540
shaykeman <shay@hliscorp.com>
2019-09-23 12:30:37 +0300

    CASLI-917 debug

--- application/models/tms_variables/Games.php
+++ application/models/tms_variables/Games.php
@@ -44,6 +44,7 @@ class Games extends \TMS\VariablesHolder
 //        }
         $gamesTms = new GamesTms($this->parameters["request"]);
         $result = $gamesTms->getData(GameSortCriteria::NEWEST, 1, 1);
+        var_dump($result);
         $name = !empty($result['list']->name) ? $result['list']->name : '';
 
         return $name;
