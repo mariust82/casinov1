81ef857094982e64c6e979941ab0a6756c9f4431
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-10-01 18:20:18 +0300

    refactored widgets client

---
+++ application/widgets/controllers/SpecialVideoWidgetController.php
@@ -0,0 +1,16 @@
+<?php
+namespace CMS\Widgets;
+
+require_once("hlis/cms/src/widgets/controllers/VideoController.php");
+
+class SpecialVideoWidgetController extends Controller
+{
+    protected function compile()
+    {
+        if (empty($this->parameters["url"])) {
+            throw new \CMS\Exception("Parameter is mandatory: ".$this->parameters["url"]);
+        }
+        $url = str_replace("youtu.be", "youtube.com/embed", trim(strip_tags($this->parameters["url"])));
+        return '<iframe width="500" height="300" src="'.$url.'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    }
+}
