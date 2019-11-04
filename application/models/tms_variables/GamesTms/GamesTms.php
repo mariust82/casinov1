17f0cf7592680541dffb72903c2990c3d403a595
shaykeman <shay@hliscorp.com>
2019-11-04 15:10:55 +0200

    CASLI-939 debug

--- application/models/tms_variables/GamesTms/GamesTms.php
+++ application/models/tms_variables/GamesTms/GamesTms.php
@@ -15,7 +15,6 @@ class GamesTms implements GamesTmsInterface
 
     public function getData($sortCriteria, $page=1, $limit =1)
     {   
-        var_dump($this->request->attributes("validation_results")->get("type"));
             $driver = new \CasinosLists\GamesByType(
             $this->request->attributes("validation_results")->get("type"),
             [],
