3bdeec69919d01604df12be6ad215a299042bb0d
shaykeman <shay@hliscorp.com>
2019-11-04 15:56:42 +0200

    CASLI-939 debug

--- application/models/tms_variables/Games.php
+++ application/models/tms_variables/Games.php
@@ -32,7 +32,7 @@ class Games extends \TMS\VariablesHolder
 
     public function getNewestGameInCurrentList()
     {
-        $request = (array) $this->parameters["request"];
+        $request = $this->parameters["request"];
         $gamesTms = new GamesTms($request);
         $result = $gamesTms->getData(GameSortCriteria::NEWEST, 1, 1);
         $name = !empty($result['list'][0]->name) ? $result['list'][0]->name : '';
