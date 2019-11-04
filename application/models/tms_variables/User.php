12a866a1903768edc6354c60b3803669b4371780
shaykeman <shay@hliscorp.com>
2019-11-04 16:18:50 +0200

    CASLI-939 debug

--- application/models/tms_variables/User.php
+++ application/models/tms_variables/User.php
@@ -3,7 +3,6 @@ class User extends \TMS\VariablesHolder
 {
     public function getCountry()
     {
-        var_dump($this->parameters["response"]->attributes("country"));
         return $this->parameters["response"]->attributes("country")->name;
     }
 }
