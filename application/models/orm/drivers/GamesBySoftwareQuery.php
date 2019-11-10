2f3168bad1b7fd8deef7b2d07b5e3d3eec0442b5
shaykeman <shay@hliscorp.com>
2019-11-10 11:24:01 +0200

    casli-991 debug

---
+++ application/models/orm/drivers/GamesBySoftwareQuery.php
@@ -0,0 +1,11 @@
+<?php
+namespace CasinosLists;
+
+class GamesBySoftwareQuery extends \Hlis\GameListRangeQuery
+{
+    protected function setWhere()
+    {
+        $where = $this->select->where();
+        $where->set("t2.id", $this->condition->manufacturer);
+    }
+}
