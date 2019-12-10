e934e38248168c90c8bf6d9476fa84055529d919
shaykeman <shay@hliscorp.com>
2019-12-10 13:32:16 +0200

    debug rmv

--- application/models/validators/StringValidator.php
+++ application/models/validators/StringValidator.php
@@ -4,6 +4,7 @@ class StringValidator extends \Lucinda\RequestValidator\ParameterValidator
 {
     public function validate($value)
     {
+        var_dump($value);
         return is_string($value) ? ($value != '' ? $value : true) : null;
     }
 }
