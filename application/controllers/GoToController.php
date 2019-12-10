cdb84337ca6d6269323c7470c6436bdf6091e4e7
shaykeman <shay@hliscorp.com>
2019-12-10 13:45:28 +0200

    debug

--- application/controllers/GoToController.php
+++ application/controllers/GoToController.php
@@ -5,6 +5,7 @@ class GoToController extends Controller
 {
     public function run()
     {
+        var_dump($this->request->getValidator()->getPathParameter("url"));
 	redirect($this->request->getValidator()->getPathParameter("url"), $this->request->getParameter("utm_medium"));
     }
 }
