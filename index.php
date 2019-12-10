0f55de38a8ff280568864db3df347ad2100fe183
shaykeman <shay@hliscorp.com>
2019-12-10 13:51:22 +0200

    debug

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 require_once("hlis/firewall/Manager.php");
-var_dump('test');
+
 // performs environment detection
 $environment = getenv("ENVIRONMENT");
 if(!$environment) die("Value of environment variable 'ENVIRONMENT' could not be detected!");
 define("ENVIRONMENT", $environment);
-var_dump('test1');
+
 // takes control of STDERR
 require_once("vendor/lucinda/errors-mvc/src/FrontController.php");
 require_once("application/models/EmergencyHandler.php");
 new Lucinda\MVC\STDERR\FrontController((ENVIRONMENT!="frontend"?"stderr.xml":"stderr_frontend.xml"), ENVIRONMENT, __DIR__, new EmergencyHandler());
-var_dump('test2');
+
 // takes control of STDOUT
 require_once("vendor/lucinda/mvc/loader.php");
 new Lucinda\MVC\STDOUT\FrontController((ENVIRONMENT!="frontend"?"stdout.xml":"stdout_frontend.xml"));
-var_dump('test3');
