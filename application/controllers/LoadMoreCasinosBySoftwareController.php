a21c28d4b4c62a777d3a8b861991c3efa2b7b6d8
Shay Dadon <shay@hliscorp.com>
2019-12-25 15:28:59 +0200

    fix2

--- application/controllers/LoadMoreCasinosBySoftwareController.php
+++ application/controllers/LoadMoreCasinosBySoftwareController.php
@@ -1,5 +1,6 @@
 <?php
 require_once("application/models/CasinoSortCriteria.php");
+require_once("application/models/CasinoFilter.php");
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
