cb813d08d4ffdc16700d0af97a66ec526a8a4e6a
shaykeman <shay@hliscorp.com>
2020-01-16 15:25:48 +0200

    CASLI-1135 debug

--- application/models/dao/BankingMethods.php
+++ application/models/dao/BankingMethods.php
@@ -16,8 +16,8 @@ class BankingMethods implements CasinoCounter
     public function getPopularBankingCasinosCount($banking)
     {
         $query = "SELECT name, COUNT(DISTINCT casino_id) AS nr FROM
-            ( SELECT t1.casino_id, t1.banking_method_id,t3.name FROM casinos__withdraw_methods AS t1 INNER JOIN banking_methods AS t3 ON (t1.banking_method_id = t3.id) INNER JOIN casinos AS t2 ON t2.id = t1.casino_id WHERE t2.is_open=1 AND t3.name != {$banking}
-            UNION SELECT t1.casino_id, t1.banking_method_id,t3.name FROM casinos__deposit_methods AS t1 INNER JOIN banking_methods AS t3 ON (t1.banking_method_id = t3.id) INNER JOIN casinos AS t2 ON t2.id = t1.casino_id WHERE t2.is_open=1  AND t3.name != {$banking}) AS alias 
+            ( SELECT t1.casino_id, t1.banking_method_id,t3.name FROM casinos__withdraw_methods AS t1 INNER JOIN banking_methods AS t3 ON (t1.banking_method_id = t3.id) INNER JOIN casinos AS t2 ON t2.id = t1.casino_id WHERE t2.is_open=1 AND t3.name !='{$banking}'
+            UNION SELECT t1.casino_id, t1.banking_method_id,t3.name FROM casinos__deposit_methods AS t1 INNER JOIN banking_methods AS t3 ON (t1.banking_method_id = t3.id) INNER JOIN casinos AS t2 ON t2.id = t1.casino_id WHERE t2.is_open=1  AND t3.name != '{$banking}') AS alias 
             GROUP BY banking_method_id ORDER BY priority DESC, nr DESC LIMIT 3
         ";
             echo $query;
