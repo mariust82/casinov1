a4f6eac58a03eae7542458f5b0bb99f641bd5053
CasinosLists <server@hliscorp.com>
2020-02-03 09:19:11 -0500

    updated frontend

--- application/controllers_frontend/ArticleController.php
+++ application/controllers_frontend/ArticleController.php
@@ -1,5 +1,5 @@
 <?php
-class GameTypesController extends Lucinda\MVC\STDOUT\Controller {
+class ArticleController extends Lucinda\MVC\STDOUT\Controller {
     public function run() {
         $this->response->attributes("country", array (
   'id' => '43',
@@ -461,7 +461,7 @@ $this->response->attributes("menu_top", array (
   array (
     'title' => 'GAMES',
     'url' => '/games',
-    'is_active' => true,
+    'is_active' => false,
     'submenuItems' => 
     array (
       0 => 
@@ -588,7 +588,7 @@ $this->response->attributes("menu_top", array (
       array (
         'title' => 'All Games',
         'url' => '/games',
-        'is_active' => true,
+        'is_active' => false,
         'submenuItems' => 
         array (
         ),
@@ -601,7 +601,7 @@ $this->response->attributes("menu_top", array (
   array (
     'title' => 'BLOG',
     'url' => '/blog',
-    'is_active' => false,
+    'is_active' => true,
     'submenuItems' => 
     array (
       0 => 
@@ -638,76 +638,324 @@ $this->response->attributes("menu_top", array (
     'have_submenu' => true,
   ),
 ));
-$this->response->attributes("results", array (
-  'Video Slots' => '3079',
-  'Slots' => '289',
-  'Video Poker' => '287',
-  'Scratch Cards' => '155',
-  'Blackjack' => '112',
-  'Roulette' => '109',
-  'Table Games' => '99',
-  'Other' => '91',
-  'Bingo' => '26',
-  'Keno' => '26',
-  'Baccarat' => '20',
-  'Craps' => '10',
+$this->response->attributes("article", array (
+  'id' => '73',
+  'title' => 'widget admin new features',
+  'type' => 'guides',
+  'description' => '<h2 style="font-style: italic;">1. Quote</h2>
+
+<blockquote>
+<p style="color: rgb(170, 170, 170); font-style: italic;"><span style="color:#000000;">This is Quote section.<br />
+This is Quote section.<br />
+This is Quote section.<br />
+This is Quote section.</span></p>
+</blockquote>
+
+<h2 style="font-style:italic;">2. Table</h2>
+
+<table align="left" border="1" cellpadding="1" cellspacing="1" dir="ltr" style="width:100%;">
+	<thead>
+		<tr>
+			<th scope="col">PLAYER THIRD CARD</th>
+			<th scope="col">BANKER</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>8</td>
+			<td>0 - 2</td>
+		</tr>
+		<tr>
+			<td>6 or 7</td>
+			<td>0 - 6</td>
+		</tr>
+		<tr>
+			<td>4 or 5</td>
+			<td>0 - 4</td>
+		</tr>
+	</tbody>
+</table>
+
+<p> </p>
+
+<h2>3. Horizontal Line</h2>
+
+<hr />
+<h2>4. Special Block Text</h2>
+
+<div class="spec_div" id="spec_div" style="background:#eeeeee;border:1px solid #cccccc;padding:5px 10px;">
+<h2>Hello</h2>
+
+<p>Hello this is Special Block Text</p>
+</div>
+
+<p> </p>
+',
+  'date_added' => '2020-01-20 06:01:45',
+  'min_read' => '10',
+  'rating' => 
+  array (
+    'likes' => '1',
+    'dislikes' => '1',
+  ),
+  'titleImageDesktop' => '/home/casinoslists/public_html/dev/site/upload/73/1.png',
+  'titleImageMobile' => '/home/casinoslists/public_html/dev/site/upload/73/3.png',
+  'thumbnail' => '/home/casinoslists/public_html/dev/site/upload/73/2.png',
 ));
-$this->response->attributes("logos", array (
-  0 => '/public/sync/game_type_logo/136x100/video_slots.jpg',
-  1 => '/public/sync/game_type_logo/136x100/slots.jpg',
-  2 => '/public/sync/game_type_logo/136x100/video_poker.jpg',
-  3 => '/public/sync/game_type_logo/136x100/scratch_cards.jpg',
-  4 => '/public/sync/game_type_logo/136x100/blackjack.jpg',
-  5 => '/public/sync/game_type_logo/136x100/roulette.jpg',
-  6 => '/public/sync/game_type_logo/136x100/table_games.jpg',
-  7 => '/public/sync/game_type_logo/136x100/other.jpg',
-  8 => '/public/sync/game_type_logo/136x100/bingo.jpg',
-  9 => '/public/sync/game_type_logo/136x100/keno.jpg',
-  10 => '/public/sync/game_type_logo/136x100/baccarat.jpg',
-  11 => '/public/sync/game_type_logo/136x100/craps.jpg',
+$this->response->attributes("tags", array (
 ));
+$this->response->attributes("tms_article", array (
+  'id' => '73',
+  'name' => 'widget admin new features',
+  'readingTime' => '10',
+  'content' => '<h2 style="font-style: italic;">1. Quote</h2>
+
+<blockquote>
+<p style="color: rgb(170, 170, 170); font-style: italic;"><span style="color:#000000;">This is Quote section.<br />
+This is Quote section.<br />
+This is Quote section.<br />
+This is Quote section.</span></p>
+</blockquote>
+
+<h2 style="font-style:italic;">2. Table</h2>
+
+<table align="left" border="1" cellpadding="1" cellspacing="1" dir="ltr" style="width:100%;">
+	<thead>
+		<tr>
+			<th scope="col">PLAYER THIRD CARD</th>
+			<th scope="col">BANKER</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>8</td>
+			<td>0 - 2</td>
+		</tr>
+		<tr>
+			<td>6 or 7</td>
+			<td>0 - 6</td>
+		</tr>
+		<tr>
+			<td>4 or 5</td>
+			<td>0 - 4</td>
+		</tr>
+	</tbody>
+</table>
+
+<p> </p>
+
+<h2>3. Horizontal Line</h2>
+
+<hr />
+<h2>4. Special Block Text</h2>
+
+<div class="spec_div" id="spec_div" style="background:#eeeeee;border:1px solid #cccccc;padding:5px 10px;">
+<h2>Hello</h2>
+
+<p>Hello this is Special Block Text</p>
+</div>
+
+<p> </p>
+',
+  'titleImageDesktop' => '/home/casinoslists/public_html/dev/site/upload/73/1.png',
+  'titleImageMobile' => '/home/casinoslists/public_html/dev/site/upload/73/3.png',
+  'thumbnail' => '/home/casinoslists/public_html/dev/site/upload/73/2.png',
+  'postDate' => '2020-01-20 06:01:45',
+  'routeId' => NULL,
+  'draftId' => NULL,
+));
+$this->response->attributes("related", array (
+  0 => 
+  array (
+    'id' => '75',
+    'title' => 'POPP - test',
+    'type' => 'guides',
+    'description' => '<table border="1" cellpadding="1" cellspacing="1" style="width:500px;">
+	<tbody>
+		<tr>
+			<td>cap tabel</td>
+			<td>coloana 2</td>
+		</tr>
+		<tr>
+			<td>ceva</td>
+			<td>altceva</td>
+		</tr>
+		<tr>
+			<td>ceva 2</td>
+			<td>altceva 2</td>
+		</tr>
+	</tbody>
+</table>
+
+<p>fsfsdsfdfsd</p>
+
+<ol>
+	<li>dfsdfsdfs</li>
+	<li>gfdgdf</li>
+</ol>
+
+<ul>
+	<li>vvxvxcvcxvxc</li>
+	<li>cvbvxbcbvc</li>
+	<li>bvcbvcbxvb</li>
+</ul>
+',
+    'date_added' => '2020-01-27 03:01:39',
+    'min_read' => '1',
+    'rating' => 
+    array (
+      'likes' => '0',
+      'dislikes' => '0',
+    ),
+    'titleImageDesktop' => '/home/casinoslists/public_html/dev/site/upload/75/10_Slots_For_This_Summer_image_desktop.jpg',
+    'titleImageMobile' => '/home/casinoslists/public_html/dev/site/upload/75/10_Slots_For_This_Summer_image_mobile.jpg',
+    'thumbnail' => '/home/casinoslists/public_html/dev/site/upload/75/10_Slots_For_This_Summer_thumbnail.jpg',
+  ),
+  1 => 
+  array (
+    'id' => '74',
+    'title' => 'Test Mihai over 9000',
+    'type' => 'guides',
+    'description' => '<blockquote>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.</p>
+</blockquote>
+
+<hr />
+<p> </p>
+
+<p> </p>
+
+<p> </p>
+
+<table border="1" cellpadding="1" cellspacing="1" style="width:500px;">
+	<thead>
+		<tr>
+			<th scope="col">1st collumn</th>
+			<th scope="col">2nd collumn</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td style="text-align: center;">1</td>
+			<td style="text-align: center;">2</td>
+		</tr>
+		<tr>
+			<td style="text-align: center;">3</td>
+			<td style="text-align: center;">4</td>
+		</tr>
+		<tr>
+			<td style="text-align: center;">5</td>
+			<td style="text-align: center;">6</td>
+		</tr>
+	</tbody>
+</table>
+
+<p>- Paragraph</p>
+
+<p>Counting over 7 million players so far (and their number is daily increasing) “Lucky Slots” on Facebook offer an amazing experience thanks to their excellent graphics, extraordinary sound effects, and spectacular animation. Daily rewards and weekly promotions await these players and new bonuses and bonus games are added monthly. Today, many slots game producers offer besides the usual online casinos apps, versions developed especially for Facebook or various bonuses for those playing these games via Facebook. The biggest advantage of playing lucky slots on Facebook is the fact they’re absolutely free to play, with no charge.</p>
+
+<p>- Bold</p>
+
+<p><strong>Lorem ipsum</strong></p>
+
+<p>-link</p>
+
+<p><a href="http://www.google.ro" target="_blank">Google link</a></p>
+
+<p>- Bullet list</p>
+
+<ul>
+	<li>Slots Farm – Since the farm themed games are on a rampage on Facebook, “Slots Farm” gained tons in popularity among Facebook players. Offering 20 lines, guaranteed rewards, additional features, excellent sound effects and tremendous graphics, this game will keep a higher place in tops for a long time.</li>
+	<li>My Vegas Slots – already has over 500,000 players on Facebook until now and tells the story of King Arthur and those legendary times. A VIP class, full of capturing rewards, exclusive and exquisite gifts, customized packages and much more makes this slot game one of the played on Facebook.</li>
+	<li>Lucky Slots – with over 1 million players so far, this game offers an amazing adventure to its players, daily new bonuses and constant promotions, captivating with its graphics and animation.</li>
+</ul>
+
+<p>- Special Block Text</p>
+
+<div style="background:#eeeeee;border:1px solid #cccccc;padding:5px 10px;">
+<p><span style="font-size:20px;"><span style="color:#3498db;">FUN FACTS</span></span></p>
+
+<p>Counting over 7 million players so far (and their number is daily increasing) “Lucky Slots” on Facebook offer an amazing experience thanks to their excellent graphics, extraordinary sound effects, and spectacular animation. Daily rewards and weekly promotions await these players and new bonuses and bonus games are added monthly. Today, many slots game producers offer besides the usual online casinos apps, versions developed especially for Facebook or various bonuses for those playing these games via Facebook. The biggest advantage of playing lucky slots on Facebook is the fact they’re absolutely free to play, with no charge.</p>
+</div>
+
+<p> </p>
+
+<div style="background:#eeeeee;border:1px solid #cccccc;padding:5px 10px;">
+<p> </p>
+
+<p><span style="font-size:20px;"><span style="color:#3498db;">FUN FACTS</span></span></p>
+
+<p>Counting over <strong>7 million players so far</strong> (and their number is daily increasing) “Lucky Slots” on Facebook offer an amazing experience thanks to their excellent graphics, extraordinary sound effects, and spectacular animation. Daily rewards and weekly promotions await these players and new bonuses and bonus games are added monthly. Today, many slots game producers offer besides the <span style="color:#3498db;">usual online casinos apps</span>, versions developed especially for Facebook or various bonuses for those playing these games via Facebook. The biggest advantage of playing lucky slots on Facebook is the fact they’re absolutely free to play, with no charge.</p>
+
+<ul>
+	<li><strong>Slots Farm</strong> – Since the farm themed games are on a rampage on Facebook, “Slots Farm” gained tons in popularity among Facebook players. Offering 20 lines, guaranteed rewards, additional features, excellent sound effects and tremendous graphics, this game will keep a higher place in tops for a long time.</li>
+	<li><strong>My Vegas Slots</strong> – already has over 500,000 players on Facebook until now and tells the story of King Arthur and those legendary times. A VIP class, full of capturing rewards, exclusive and exquisite gifts, customized packages and much more makes this slot game one of the played on Facebook.</li>
+	<li><strong>Lucky Slots</strong> – with over 1 million players so far, this game offers an amazing adventure to its players, daily new bonuses and constant promotions, captivating with its graphics and animation.</li>
+</ul>
+</div>
+
+<p>- Headings</p>
+
+<ol>
+	<li><p>&nbsp;</p></li>
+	<li><p>&nbsp;</p></li>
+	<li><p>&nbsp;</p></li>
+	<li><p>&nbsp;</p></li>
+	<li><p>&nbsp;</p></li>
+</ol>
+',
+    'date_added' => '2020-01-22 04:01:16',
+    'min_read' => '10',
+    'rating' => 
+    array (
+      'likes' => '1',
+      'dislikes' => '0',
+    ),
+    'titleImageDesktop' => '/home/casinoslists/public_html/dev/site/upload/74/2.desktop.jpg',
+    'titleImageMobile' => '/home/casinoslists/public_html/dev/site/upload/74/3.mobile.jpg',
+    'thumbnail' => '/home/casinoslists/public_html/dev/site/upload/74/1.thumbnail.jpg',
+  ),
+  2 => 
+  array (
+    'id' => '71',
+    'title' => 'test666',
+    'type' => 'guides',
+    'description' => '<p>ttt</p>
+',
+    'date_added' => '2019-11-06 10:11:32',
+    'min_read' => '10',
+    'rating' => 
+    array (
+      'likes' => '1',
+      'dislikes' => '0',
+    ),
+    'titleImageDesktop' => '/home/casinoslists/public_html/dev/site/upload/71/1.png',
+    'titleImageMobile' => '/home/casinoslists/public_html/dev/site/upload/71/3.png',
+    'thumbnail' => '/home/casinoslists/public_html/dev/site/upload/71/2.png',
+  ),
+));
+$this->response->attributes("uploadsFolders", array (
+  73 => '/upload/73/2.png?1580739488',
+));
+$this->response->attributes("relateduploadsFolders", array (
+  75 => '/upload/75/10_Slots_For_This_Summer_thumbnail.jpg?1580739488',
+  74 => '/upload/74/1.thumbnail.jpg?1580739488',
+  71 => '/upload/71/2.png?1580739488',
+));
+$this->response->attributes("title_image_thumbnail", '/upload/73/2.png?1580739488');
+$this->response->attributes("title_image_desktop", '/upload/73/1.png?1580739488');
+$this->response->attributes("title_image_mobile", '/upload/73/3.png?1580739488');
 $this->response->attributes("page_info", array (
-  'head_title' => 'Casino Games Categories | Slots, Blackjack, Roulette and more',
-  'head_description' => 'The complete list of online casino games types. With so many games available, we divided them into categories. Play Video Slots, Blackjack, Roulette & more',
-  'body_title' => 'Casino Games Types',
+  'head_title' => 'widget admin new features | CasinosLists.com',
+  'head_description' => 'widget admin new features Read top online casinos blog articles at CasinosLists.com!',
+  'body_title' => 'widget admin new features',
 ));
 $this->response->attributes("is_mobile", false);
 $this->response->attributes("version", '0.8.8.9129');
 $this->response->attributes("use_bundle", true);
 $this->response->attributes("tms", array (
-  'upper_text' => '<p>Total number of games on site : 6296</p>
-
-<p>Total number of games in the current list: </p>
-
-<p>Newest game on site  :  Majestic Sea</p>
-
-<p>Newest game in the current list : test2</p>
-
-<p>Software of the newest game on site : NetEnt</p>
-
-<p>Software of the newest game in the current list : RTG</p>
-
-<p>Most popular game on site : Exotic Fruit Deluxe</p>
-
-<p>Most popular game in the current list: Exotic Fruit Deluxe</p>
-
-<p>Software of the most popular game on site : Booming Games</p>
-
-<p>Software of the most popular game in the current list : Booming Games</p>
-
-<p>games - update</p>
-
-<p><b>0</b></p>
-
-<p><strong>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &#39;Content here, content here&#39;, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &#39;lorem ipsum&#39; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</strong></p>
-',
 ));
 $this->response->attributes("widgets", array (
-  'upper' => 
-  array (
-    0 => '<p>video slots upper text search</p>
-',
-  ),
 ));
 
     }
