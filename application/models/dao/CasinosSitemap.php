6a9119d771538002e81a85bb9671c798b999e333
shaykeman <shay@hliscorp.com>
2020-02-04 12:20:34 +0200

    CASLI-1176

--- application/models/dao/CasinosSitemap.php
+++ application/models/dao/CasinosSitemap.php
@@ -33,7 +33,7 @@ class CasinosSitemap {
                 $now = date("Y-m-d");
                 $date = strtotime($now . ' -1 year');
                 $last = date('Y-m-d', $date);
-                $output[$label] = SQL("SELECT MAX(t1.date) FROM casinos AS t1 LEFT OUTER JOIN casinos__countries_allowed AS t2 ON t1.id = t2.casino_id AND t2.country_id = {$this->country} WHERE t1.is_open = 1 AND t1.date_established > '{$last}' ORDER BY t1.date_established DESC, t1.priority DESC, t1.id DESC")->toValue();
+                $output[$label] = explode(' ',SQL("SELECT MAX(t1.date) FROM casinos AS t1 LEFT OUTER JOIN casinos__countries_allowed AS t2 ON t1.id = t2.casino_id AND t2.country_id = {$this->country} WHERE t1.is_open = 1 AND t1.date_established > '{$last}' ORDER BY t1.date_established DESC, t1.priority DESC, t1.id DESC")->toValue())[0];
             } else {
                 switch ($label) {
                     case 'Best':
@@ -49,7 +49,7 @@ class CasinosSitemap {
                         $order = ' ORDER BY t1.priority DESC, t1.id DESC ';
                         break;
                 }
-                $output[$label] = SQL("SELECT MAX(t1.date) FROM casinos AS t1 LEFT OUTER JOIN casinos__countries_allowed AS t2 ON t1.id = t2.casino_id AND t2.country_id = {$this->country} INNER JOIN casinos__labels AS t5 ON t1.id = t5.casino_id AND t5.label_id = (SELECT id FROM casino_labels WHERE name = '{$label}') WHERE t1.is_open = 1 {$order}")->toValue();
+                $output[$label] = explode(' ',SQL("SELECT MAX(t1.date) FROM casinos AS t1 LEFT OUTER JOIN casinos__countries_allowed AS t2 ON t1.id = t2.casino_id AND t2.country_id = {$this->country} INNER JOIN casinos__labels AS t5 ON t1.id = t5.casino_id AND t5.label_id = (SELECT id FROM casino_labels WHERE name = '{$label}') WHERE t1.is_open = 1 {$order}")->toValue())[0];
             }
         }
         array_multisort($output, SORT_DESC);
---
+++ application/models/dao/CountriesSitemap.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GameManufacturersSitemap
+ *
+ * @author matan
+ */
+class CountriesSitemap {
+    
+    private $country;
+    
+    public function __construct($country) {
+        $this->country = $country;
+    }
+    
+    private function setCountriesRows() {
+        $output = [];
+        $countries = $this->getAll();
+        foreach ($countries as $value) {
+            $date = SQL("SELECT MAX(t1.date) FROM casinos AS t1 LEFT OUTER JOIN casinos__countries_allowed AS t2 ON t1.id = t2.casino_id AND t2.country_id = {$this->country} INNER JOIN casinos__countries_allowed AS t7 ON t1.id = t7.casino_id AND t7.country_id = (SELECT id FROM countries WHERE name = '{$value}') AND t1.is_open = 1")->toValue();
+            $output[$value] = explode(' ', $date)[0];
+        }
+        array_multisort($output, SORT_DESC);
+        return $output;
+    }
+    
+    public function getCountriesRows() {
+        return $this->setCountriesRows();
+    }
+    
+       public function getAll()
+    {
+        return SQL("
+        SELECT DISTINCT t1.name
+        FROM countries AS t1
+        INNER JOIN casinos__countries_allowed AS t2 ON t1.id = t2.country_id
+        INNER JOIN casinos AS t3 ON t2.casino_id = t3.id
+        WHERE t3.is_open = 1
+        ORDER BY t1.name ASC 
+        ")->toColumn();
+    }
+    
+}
