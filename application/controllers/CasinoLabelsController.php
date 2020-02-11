ff54b9a2fe139aef9c3d8a20a21d28d083ffec3c
shaykeman <shay@hliscorp.com>
2020-02-11 18:13:54 +0200

    CASLI-1090 step 1

--- application/controllers/CasinoLabelsController.php
+++ application/controllers/CasinoLabelsController.php
@@ -1,5 +1,8 @@
 <?php
 require_once("application/models/dao/CasinoLabels.php");
+require_once("application/models/CasinoSortCriteria.php");
+require_once("application/models/CasinoFilter.php");
+require_once("application/models/dao/CasinosList.php");
 require_once("CasinosCounterController.php");
 /*
 * Casino labels list by number of casinos.
@@ -10,8 +13,33 @@ require_once("CasinosCounterController.php");
 */
 class CasinoLabelsController extends CasinosCounterController
 {
+    
+    private $limit;
+
     protected function getCounter()
     {
         return new CasinoLabels();
+    }
+    
+    protected function init() {
+        $this->limit = 30;
+        $results = $this->getCasinos();
+        $this->response->attributes("total_casinos", $results["total"]);
+        $this->response->attributes("casinos", $results["list"]);
+        $this->response->attributes("page_type", 'label');
+        $this->response->attributes("selected_entity", "all");
+    }
+
+    private function getCasinos()
+    {
+        $filter = new CasinoFilter(
+            [],
+            $this->request->attributes("country")
+        );
+        $object = new CasinosList($filter);
+        $results = array();
+        $results["total"] = $object->getTotal();
+        $results["list"] = ($results["total"]>0 ? $object->getResults(CasinoSortCriteria::NONE, 1, $this->limit) : array());
+        return $results;
     }
 }
