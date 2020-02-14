c15ad186483e4f42915988f8502f357629ceddf2
CasinosLists <server@hliscorp.com>
2020-02-14 09:35:37 -0500

    upgraded view language

--- application/resolvers/ViewLanguageResolver.php
+++ application/resolvers/ViewLanguageResolver.php
@@ -6,25 +6,55 @@ require_once("vendor/lucinda/framework-engine/src/view_language/ViewLanguageBind
  * View resolver for HTML format binding STDOUT MVC with View Language API and contents of 'application' tag @ configuration.xml
  * in order to be able to perform templating in a view
  */
-class ViewLanguageResolver extends \Lucinda\MVC\STDOUT\ViewResolver
+class ViewLanguageResolver extends \Lucinda\MVC\STDOUT\ViewResolver implements \Lucinda\MVC\STDERR\ErrorHandler
 {
+    /**
+     * @var \Lucinda\MVC\STDERR\ErrorHandler
+     */
+    private $defaultErrorHandler;
+
     /**
      * {@inheritDoc}
      * @see \Lucinda\MVC\STDOUT\ViewResolver::getContent()
      */
     public function getContent()
     {
+        $this->defaultErrorHandler = \Lucinda\MVC\STDERR\PHPException::getErrorHandler();
+
         // converts view language to PHP
         $wrapper = new Lucinda\Framework\ViewLanguageBinder($this->application->getTag("application"), $this->response->getView());
         $compilationFile = $wrapper->getCompilationFile();
-        
+
+        // take control of error handling
+        \Lucinda\MVC\STDERR\PHPException::setErrorHandler($this);
+        set_exception_handler(array($this,"handle"));
+
         // compiles PHP file into output buffer
         $data = $this->response->attributes();
         ob_start();
         require_once($compilationFile);
         $output = ob_get_contents();
         ob_end_clean();
-        
+
+        // restores default error handler
+        \Lucinda\MVC\STDERR\PHPException::setErrorHandler($this->defaultErrorHandler);
+        set_exception_handler(array($this->defaultErrorHandler,"handle"));
+
         return $output;
     }
+
+    /**
+     * Handles errors by delegating to STDOUT MVC API
+     *
+     * @param \Exception $exception Encapsulates error information.
+     */
+    public function handle($exception)
+    {
+        // close output buffer
+        ob_end_clean();
+
+        // delegate handling to STDERR MVC API
+        $this->defaultErrorHandler->handle($exception);
+    }
 }
+
