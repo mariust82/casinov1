ab48ba579a5158cef9c493e0d32efa0a45e8f663
CasinosLists <server@hliscorp.com>
2020-02-21 05:29:11 -0500

    widgets integration

--- application/models/widgets/parameters/input/FAQ.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace CasinosLists\Parameter\Input;
-
-class FAQ extends \CMS\WidgetParameter
-{
-    public $disabled = false;
-    public $readonly = false;
-    public $required = false;
-}
