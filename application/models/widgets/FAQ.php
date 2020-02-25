e3d24e768577a7f94ef13bd958b9021299943d65
shaykeman <shay@hliscorp.com>
2020-02-25 14:02:18 +0200

    test

--- application/models/widgets/FAQ.php
+++ application/models/widgets/FAQ.php
@@ -1,2 +1,28 @@
 <?php
-require_once(dirname(dirname(dirname(__DIR__)))."/hlis/widgets/src/widgets/FAQ.php");
+namespace CasinosLists;
+
+class FAQ extends \CMS\FAQ
+{
+    public function render($remoteID, $extraBindings=array())
+    {
+        try {
+            $info = $this->get($remoteID);
+        } catch (\Exception $e) {}
+        if (empty($info)) {
+            return "";
+        }
+
+        $variables= new Variables($this->variablesFolder, $extraBindings);
+        $output = '<div class="widget faq">';
+        $output.= '<h2>';
+        $output .= $variables->process($info[0]->value);
+        $output.= '</h2>';
+        $tableReader = new TableReader($info[1]->value);
+        $rows = $tableReader->getRows();
+        foreach($rows as $row) {
+            $output .= '<div><div>'.$variables->process($row[0]).'</div><div>'.$variables->process($row[1]).'</div></div>';
+        }
+        $output .= '</div>';
+        return $output;
+    }
+}
