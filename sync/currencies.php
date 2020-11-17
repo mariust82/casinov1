33aef6cbcb4f7d23d7e9c674ef3e9cf8cfa30521
shaykeman <shay@hliscorp.com>
2020-11-17 09:35:04 +0200

    CASLI-1408

--- sync/currencies.php
+++ sync/currencies.php
@@ -1,4 +1,4 @@
 <?php
 set_include_path(dirname(__DIR__)."/hlis/sync");
 require_once("src/controllers/CurrenciesSynchronization.php");
-new CurrenciesSynchronization(dirname(dirname(__FILE__))."/stdout.xml");
+new CurrenciesSynchronization(__DIR__."/stdout.xml");
--- sync/user_preferences.php
+++ sync/user_preferences.php
@@ -1,4 +1,4 @@
 <?php
 set_include_path(dirname(__DIR__)."/hlis/sync");
 require_once("src/controllers/UserPreferencesSynchronization.php");
-new UserPreferencesSynchronization(dirname(__DIR__)."/stdout.xml");
+new UserPreferencesSynchronization(__DIR__."/stdout.xml");
