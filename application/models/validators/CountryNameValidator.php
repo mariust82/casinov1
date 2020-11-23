15828dfde67d587598207c7835a4719aaa185d18
shaykeman <shay@hliscorp.com>
2020-11-23 16:25:55 +0200

    CASLI-1609

--- application/models/validators/CountryNameValidator.php
+++ application/models/validators/CountryNameValidator.php
@@ -8,10 +8,36 @@ class CountryNameValidator extends \Lucinda\RequestValidator\ParameterValidator
             return null;
         }
 
-        if (!in_array($value, array("guinea-bissau","timor-leste"))) {
+       
+        
+        switch ($value) {
+            case "holy-see-vatican-city-state":
+                $value = "holy-see-(vatican-city-state)";
+                break;
+            case "sint-maarten-dutch-part":
+                $value = "sint-maarten-(dutch-part)";
+                break;
+            case "democratic-peoples-republic-of-korea":
+                $value = "democratic-people's-republic-of-korea";
+                break;
+            case "cocos-keeling-islands":
+                $value = "cocos-(keeling)-islands";
+                break;
+            case "falkland-islands-malvinas":
+                $value = "falkland-islands-malvinas";
+                break;
+            case "cote-divoire":
+                $value = "cote-d'ivoire";
+                break;
+            case "lao-peoples-democratic-republic":
+                $value = "lao-people's-democratic-republic";
+                break;
+        }
+        
+         if (!in_array($value, array("guinea-bissau","timor-leste"))) {
             $value = str_replace("-", " ", $value);
         }
-
+        
         $id =  SQL("SELECT id FROM countries WHERE name=:name", array(":name"=>$value))->toValue();
 
         return !empty($id) ? $id : null;
