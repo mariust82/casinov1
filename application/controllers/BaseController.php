334c069c8e945c68d269904a245065613a3839e2
bcazan <bcazan@hotmail.com>
2020-12-07 16:27:45 +0200

    updt

--- application/controllers/BaseController.php
+++ application/controllers/BaseController.php
@@ -8,10 +8,7 @@ abstract class BaseController extends Lucinda\MVC\STDOUT\Controller
     public function run()
     {
         $specificPage = $this->request->getURI()->getPage();
-        if($specificPage == 'games/slots') {
-            $this->response->setStatus(301);
-            $this->response->redirect('/games/classic-slots');
-        }
+
         if($specificPage == 'casinos/popular') {
             $this->response->setStatus(301);
             $this->response->redirect('/casinos/best');
