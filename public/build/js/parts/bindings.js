ffec839e79eecf1e8159c9202d3c283e35d15c14
Bianca Draghici <bianca.d@gtmarketing.ro>
2020-12-15 12:45:46 +0200

    test

--- public/build/js/parts/bindings.js
+++ public/build/js/parts/bindings.js
@@ -72,6 +72,16 @@ contentTooltipConfigPopup = {
 
 function contentTooltipConfigPopupActions(origin) {
     checkStringLength($('.bonus-box'), 15);
+    if ($(window).width() > 767) {
+        $('.bonus-info .content_popup').niceScroll({
+            cursorcolor: "#A8AEC8",
+            cursorwidth: "3px",
+            autohidemode: false,
+            cursorborder: "1px solid #A8AEC8",
+            railoffset: { top: 0, left: 20 },
+            horizrailenabled: false,
+        });
+    }
     $('.js-tooltip').tooltipster(tooltipConfig);
     $('.js-copy-tooltip').tooltipster(copyTooltipConfig);
     copyToClipboard();
