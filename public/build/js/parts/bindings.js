6edb88650bee12ffd711be7aa62a74bfa68e53e8
Bianca Draghici <bianca.d@gtmarketing.ro>
2020-12-15 09:04:42 +0200

    testing

--- public/build/js/parts/bindings.js
+++ public/build/js/parts/bindings.js
@@ -1556,7 +1556,7 @@ var SearchPanel = function (obj) {
 };
 
 var initSite = function () {
-
+    console.log('test here');
     copyToClipboard();
     checkStringLength($('.list .bonus-box'), 21);
     checkStringLength($('.bonus-item .bonus-box'), 33);
