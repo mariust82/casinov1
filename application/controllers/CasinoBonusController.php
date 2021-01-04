831f54588e7ce9f5dbd1106f109a0b2ecc96f0c2
bcazan <bcazan@hotmail.com>
2021-01-04 14:42:12 +0200

    CASLI-1725

--- application/controllers/CasinoBonusController.php
+++ application/controllers/CasinoBonusController.php
@@ -1,4 +1,5 @@
 <?php
+require_once("application/models/dao/CasinoInfo.php");
 require_once("application/models/dao/Casinos.php");
 
 class CasinoBonusController extends Lucinda\MVC\STDOUT\Controller
@@ -7,6 +8,8 @@ class CasinoBonusController extends Lucinda\MVC\STDOUT\Controller
     {
         $object = new Casinos();
         $casinoID = $this->request->attributes('validation_results')->get('casino');
+        $casinoInfo = new CasinoInfo($casinoID, $this->request->attributes("country")->id);
+        $this->response->attributes("casino", $casinoInfo->getResult());
         $this->response->attributes("is_mobile", $this->request->attributes("is_mobile"));
         $this->response->attributes("name", $this->request->parameters("casino"));
         $this->response->attributes("bonus", $object->getBonus($casinoID, (boolean) $this->request->parameters("is_free")));
