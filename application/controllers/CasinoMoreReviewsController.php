28cc29752556db8a602f6f85597424e98049dcf9
Shay Dadon <shay@hliscorp.com>
2021-01-20 15:26:38 +0200

    fix

--- application/controllers/CasinoMoreReviewsController.php
+++ application/controllers/CasinoMoreReviewsController.php
@@ -2,7 +2,7 @@
 require_once("application/models/dao/CasinoReviews.php");
 require_once("application/models/dao/Casinos.php");
 require_once("application/models/CasinoScore.php");
-
+require_once("application/models/dao/CasinoInfo.php");
 /*
 * Gets more reviews/replies on a casino.
 *
@@ -34,6 +34,9 @@ class CasinoMoreReviewsController extends Lucinda\MVC\STDOUT\Controller
         } else {
             $this->response->attributes("reviews", $object->getMoreReplies((integer) $this->request->getValidator()->parameters("page"), (integer) $this->request->parameters("id")));
         }
+        $object = new CasinoInfo($this->request->attributes('validation_results')->get('name'), $this->request->attributes("country")->id);
+        $info = $object->getResult();
+        $this->response->attributes("casino", (array) $info);
         $this->response->attributes("total_reviews", $total);
         $this->response->attributes("total_votes", $total_votes);
         $this->response->attributes("user_votes", $casino_score->setVotesByType($votes));
