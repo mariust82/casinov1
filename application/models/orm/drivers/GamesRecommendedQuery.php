40e226dbf92bc6ac967aa63bc7afd6d6772a3194
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2021-01-25 13:24:34 +0200

    CASLI-1823

--- application/models/orm/drivers/GamesRecommendedQuery.php
+++ application/models/orm/drivers/GamesRecommendedQuery.php
@@ -3,6 +3,22 @@ namespace CasinosLists;
 
 class GamesRecommendedQuery extends \Hlis\GameListRangeQuery
 {
+    public function __construct(GameFields $fields, GameCondition $condition, GameSort $sort, $offset, $limit) {
+        $this->fields = $fields->getCriteria();
+        $this->condition = $condition->getCriteria();
+        $this->sort = $sort->getCriteria();
+        $this->sortOrder = $sort->getOrder();
+        $this->offset = $offset;
+        $this->limit = $limit;
+        
+        $this->select = new \Lucinda\Query\MySQLSelect("games", "t1");
+        $this->select->setStraightJoin();
+        $this->setFields();
+        $this->setJoins();
+        $this->setWhere();
+        $this->setOrderBy();
+        $this->setLimit();
+    }
     protected function setWhere()
     {
         $where = $this->select->where();
