2a177a599f73d06a879c230c6ea006bbd2a0e8aa
Marius <marius.s@gtmarketing.ro>
2021-02-10 15:58:07 +0200

    1682

---
+++ application/models/dao/entities/MatchBonus.php
@@ -0,0 +1,10 @@
+<?php
+class MatchBonus extends Entity
+{
+    public $valid_on;
+    public $bonus;
+    public $min_deposit;
+    public $wagering;
+    public $games;
+    public $bonus_codes;
+}
