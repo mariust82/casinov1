a77f1c4deecde4507522e7a03b63201a9d76e7a4
shaykeman <shay@hliscorp.com>
2021-02-18 10:03:21 +0200

    fix country flag slider

--- application/models/dao/Countries.php
+++ application/models/dao/Countries.php
@@ -84,7 +84,7 @@ class Countries implements CasinoCounter
     public function getCountryInfo($id)
     {
         return SQL("
-            SELECT t1.id AS currency_id, t2.id AS lang_id, t1.code, t2.name, t3.name as c_name from countries AS t3 
+            SELECT t1.id AS currency_id, t2.id AS lang_id, t1.code, t2.name, t3.name as c_name, t3.code AS c_code from countries AS t3 
               INNER JOIN countries__languages AS t4 ON (t3.id = t4.country_id)
               INNER JOIN currencies AS t1 ON (t1.id = t3.currency_id)
               INNER JOIN languages AS t2 ON (t2.id = t4.language_id) 
