0c6344ce7de64b9b70ef3229404c730b42b71309
Bianca Draghici <bianca.d@gtmarketing.ro>
2021-02-19 15:31:24 +0200

    test

--- public/build/js/parts/bindings.js
+++ public/build/js/parts/bindings.js
@@ -1824,3 +1824,35 @@ var initSite = function () {
         e.preventDefault();
     });
 }
+
+
+function similarCasinosSlider(){
+    const slider = document.querySelector('.similar_casinos-slider');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1;
+    slider.scrollLeft = scrollLeft - walk;
+    });
+}
+
+similarCasinosSlider();
