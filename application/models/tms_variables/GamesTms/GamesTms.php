240b54abae23e35dbf3bf6d3d45b00849d51c149
vadymg <vadym.gerega@gmail.com>
2021-04-01 16:22:32 +0300

    CASLI-2186 - Make pages display in less than 0.25s

--- application/models/tms_variables/GamesTms/GamesTms.php
+++ application/models/tms_variables/GamesTms/GamesTms.php
@@ -4,26 +4,17 @@ require_once 'application/models/GameSortCriteria.php';
 require_once("application/models/orm/GamesByType.php");
 
 
-
 class GamesTms implements GamesTmsInterface
 {
     private $request;
+
     public function __construct($request)
     {
         $this->request = $request;
     }
 
-    public function getData($sortCriteria, $page=1, $limit =1)
-    {   
-            $driver = new \CasinosLists\GamesByType(
-            $this->request->attributes("validation_results")->get("type"),
-            [],
-            $this->request->attributes("is_mobile"),
-            $sortCriteria?$sortCriteria:GameSortCriteria::NONE,
-            $page,
-            $limit
-        );
-        $result = $driver->getResults();
-        return $result;
+    public function getData($sortCriteria, $page = 1, $limit = 1)
+    {
+        return [];
     }
 }
