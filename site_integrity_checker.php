ea16f7a0b3b6dcf15d82be60824f745e19bfeef8
luciangreentree <lucian@hliscorp.com>
2021-04-07 18:10:49 +0300

    Update site_integrity_checker.php

--- site_integrity_checker.php
+++ site_integrity_checker.php
@@ -3,7 +3,7 @@ ini_set("display_errors", 1);
 require("vendor/autoload.php");
 require_once("hlis/unit_testing/ConsoleWrapper.php");
 
-\Hlis\Testing\PageSpeedValidator::$tolerance = 350;
+\Hlis\Testing\PageSpeedValidator::$tolerance = 400;
 $wrapper = new \Hlis\Testing\ConsoleWrapper($argv, "casinoslists");
 $wrapper->run([
     "index" => "index",
