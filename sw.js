3f314672876e9612f8739b75f265391b1ece56d3
vadymg <vadym.gerega@gmail.com>
2021-05-19 00:43:38 +0300

    Test setup PWA. Added caching part

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
 self.addEventListener('install', event => {
-    const version = event.target.location.search;
-    event.waitUntil(
-        caches.open('static')
-            .then(cache => {
-                cache.add('/');
-                cache.add('/public/build/css/compilations/all.css' + version);
-                cache.add('public/build/js/compilations/defer.js' + version);
-            })
-    );
+    console.log('SW installed...');
 });
 
 self.addEventListener('activate', event => {
@@ -15,21 +7,5 @@ self.addEventListener('activate', event => {
     return self.clients.claim();
 });
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                } else {
-                    return fetch(event.request)
-                        .then(dynamicResponse => {
-                            return caches.open('dynamic')
-                                .then(cache => {
-                                    cache.put(event.request.url, dynamicResponse.clone());
-                                    return dynamicResponse;
-                                });
-                        }).catch(error => console.log(error));
-                }
-            })
-    );
+    console.log('fetch');
 });
