4727c669aa98ba71a1d36c0ca168360110f5346c
vadymg <vadym.gerega@gmail.com>
2021-06-30 12:41:19 +0300

    CASLI-2390 - Create PWA Version.

--- sw.js
+++ sw.js
@@ -94,7 +94,6 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('[SW] V1 now ready to handle fetches!');
     event.waitUntil(
         caches.keys()
             .then((keyList) => {
